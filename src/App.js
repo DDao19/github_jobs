a48ab2c2d07c00c7b270d83dc1782305c72cfcc9
Dung Dao <dung.dao19@outlook.com>
2020-11-02 19:15:07 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import useFetchJobs from './useFetchJobs'
+import { Container } from 'react-bootstrap' 
 
-function App() {
+import Job from './components/Job'
+import JobsPagination from './components/JobsPagination'
+import SearchForm from './components/SearchForm'
+import './App.css'
+
+const App = () => {
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
+
+  const handleParamChange = (e) => {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prevPraams => {
+      return { ...prevPraams, [param]: value }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="my-4">
+      <h1 className="title"><span className="logo1">Github: </span> <span className="logo2">Jobs</span> <span className="search-logo"><i className="fas fa-search"></i></span></h1>
+      <h4 className="mb-5 text-muted font-weight-light description">Discover the latest entries from Github: Jobs!</h4>
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+      {loading && <h6>Loading....</h6>}
+      {error && <h1>Error. Try Refreshing.</h1>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+    </Container>
   );
 }
 
